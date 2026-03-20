c70abff3c54e943c2305789f4e73d5a909e48a79
David Baum <davidbaum461@gmail.com>
2026-03-20 14:29:00 -0500

    euler is garbage on sharp turns switching to rk4

--- pathracer/optimal_path.py
+++ pathracer/optimal_path.py
@@ -11,11 +11,15 @@ class FMMSolver:
         self.driveable = driveable
         ss = self.cfg.supersample
 
-        # blow up the mask so the path isnt all jaggy and pixelated
         drive_hi = np.repeat(np.repeat(driveable, ss, 0), ss, 1)
         self.phi = np.where(drive_hi, -1.0, 1.0)
         self.T = skfmm.travel_time(self.phi, np.ones_like(self.phi))
         self.Ty, self.Tx = np.gradient(self.T)
+
+    def _grad_at(self, p):
+        iy = min(max(int(p[0]), 0), self.T.shape[0] - 2)
+        ix = min(max(int(p[1]), 0), self.T.shape[1] - 2)
+        return np.array([self.Ty[iy, ix], self.Tx[iy, ix]])
 
     def compute_optimal_path(self, start_xy, goal_xy, max_iters=25000):
         ss = self.cfg.supersample
@@ -31,12 +35,16 @@ class FMMSolver:
             if np.linalg.norm(p - start_hi) < 1.0:
                 break
 
-            # basic euler step following the gradient downhill
-            iy = min(max(int(p[0]), 0), self.T.shape[0] - 2)
-            ix = min(max(int(p[1]), 0), self.T.shape[1] - 2)
-            g = np.array([self.Ty[iy, ix], self.Tx[iy, ix]])
-            g /= np.linalg.norm(g) + eps
-            p -= h * g
+            # rk4 bc euler was way too sloppy on tight corners
+            k1 = self._grad_at(p)
+            k1 /= np.linalg.norm(k1) + eps
+            k2 = self._grad_at(p - .5*h*k1)
+            k2 /= np.linalg.norm(k2) + eps
+            k3 = self._grad_at(p - .5*h*k2)
+            k3 /= np.linalg.norm(k3) + eps
+            k4 = self._grad_at(p - h*k3)
+            k4 /= np.linalg.norm(k4) + eps
+            p -= h * (k1 + 2*k2 + 2*k3 + k4) / 6
             trace.append(p[::-1])
 
         return np.asarray(trace)[::-1] / ss
