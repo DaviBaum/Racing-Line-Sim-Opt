99e820f9c73be338da38012cef1d41def965aaad
David Baum <davidbaum461@gmail.com>
2026-03-20 18:41:00 -0500

    push beads back on road when they slide off

--- pathracer/optimal_path.py
+++ pathracer/optimal_path.py
@@ -12,7 +12,9 @@ class FMMSolver:
         ss = self.cfg.supersample
 
         drive_hi = np.repeat(np.repeat(driveable, ss, 0), ss, 1)
+        # -1 = road  +1 = wall
         self.phi = np.where(drive_hi, -1.0, 1.0)
+        # fmm gives us a travel time field and then we just roll downhill on it
         self.T = skfmm.travel_time(self.phi, np.ones_like(self.phi))
         self.Ty, self.Tx = np.gradient(self.T)
 
@@ -32,10 +34,23 @@ class FMMSolver:
         bot = g01 * (1 - dy) + g11 * dy
         return top * (1 - dx) + bot * dx
 
+    def _push_onto_road(self, q):
+        # if it went off road shove it back using the gradient as a normal
+        eps = self.cfg.eps
+        rc = np.clip(np.round(q[::-1]).astype(int), [0, 0],
+                     np.array(self.phi.shape) - 1)
+        phi_val = self.phi[rc[0], rc[1]]
+        if phi_val > 0:
+            n = self._grad_at(q[::-1])
+            n /= np.linalg.norm(n) + eps
+            return q - phi_val * n[::-1]
+        return q
+
     def compute_optimal_path(self, start_xy, goal_xy, max_iters=25000):
         ss = self.cfg.supersample
         eps = self.cfg.eps
 
+        # go from goal backwards to start bc thats how fmm descent works
         start_hi = np.array(start_xy[::-1]) * ss
         goal_hi = np.array(goal_xy[::-1]) * ss
         p = goal_hi.copy()
@@ -63,6 +78,7 @@ class FMMSolver:
 
     def _relax(self, path):
         # the raw fmm path is kinda crunchy so we smooth it out
+        # treat it like a rubber band with beads on it and let it settle
         ss = self.cfg.supersample
         nb = self.cfg.string_beads
         t_in = np.linspace(0, 1, len(path))
@@ -73,10 +89,17 @@ class FMMSolver:
         ])
 
         for _ in range(self.cfg.string_iters):
+            # each bead gets pulled toward being a straight line between its neighbors
             segs = np.diff(pts, axis=0)
             lens = np.linalg.norm(segs, axis=1) + self.cfg.eps
             force = np.zeros_like(pts)
             force[1:-1] = segs[1:]/lens[1:, None] - segs[:-1]/lens[:-1, None]
             pts[1:-1] -= 0.25 * force[1:-1]
+            # shove any beads that slid off the road back on
+            pts = np.vstack([
+                pts[0],
+                [self._push_onto_road(p) for p in pts[1:-1]],
+                pts[-1],
+            ])
 
         return pts / ss
