05ecada6cf2ebcbed47bc9c69a875130dc19ded6
David Baum <davidbaum461@gmail.com>
2026-03-27 10:25:00 -0500

    init file with exports

--- pathracer/__init__.py
+++ pathracer/__init__.py
@@ -1,1 +1,8 @@
-"""PathRacer."""
+"""PathRacer - Physics-based path racing simulator."""
+
+from .config import SimConfig, DEFAULT_CONFIG
+from .centerline import ordered_centerline
+from .physics import curvature, speed_profile, resample_time
+from .optimal_path import FMMSolver
+from .animation import create_race_animation
+from .pipeline import run_race
