2329dc4e09cc7d4a173ca5c214a1ba5eef427a60
David Baum <davidbaum461@gmail.com>
2026-04-01 16:50:00 -0500

    remove old TODOs

--- pathracer/__init__.py
+++ pathracer/__init__.py
@@ -1,6 +1,6 @@
 """PathRacer - Physics-based path racing simulator."""
 
-__version__ = "0.1.0"
+__version__ = "0.9.0"
 
 from .config import SimConfig, DEFAULT_CONFIG
 from .centerline import ordered_centerline
