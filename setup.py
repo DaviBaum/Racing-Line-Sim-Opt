5ac67d0bf73eb9933dd423f659ca4968ee445de5
David Baum <davidbaum461@gmail.com>
2026-04-05 12:10:00 -0500

    cleanup

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="pathracer",
-    version="0.1.0",
+    version="0.9.0",
     description="Physics-based path racing simulator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="David Baum",
     license="MIT",
     packages=find_packages(),
@@ -13,7 +15,7 @@ setup(
         "matplotlib>=3.7",
         "scikit-image>=0.21",
         "scipy>=1.10",
-        "scikit-fmm",
+        "scikit-fmm>=2024.1",
         "Pillow>=10.0",
     ],
     entry_points={
